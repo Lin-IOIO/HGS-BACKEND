b30aadcef9b5a80d8caa49568e8aee030a5bbada
Abel <girisha.sangih@gmail.com>
2025-12-03 14:15:41 -0300

    Habilitación de cors y algo más

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const {PORT} = process.env;
 
 const app = express();
@@ -7,7 +8,8 @@ const app = express();
 app.use(express.json())
 
 const apiRouter = require('./api/main')
-
+const allowlist = ['http://localhost:5173'];
+app.use('/api', cors({origin: allowlist}));
 app.use('/api', apiRouter);
 
 app.listen(PORT, function(error) {
